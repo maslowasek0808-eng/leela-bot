cd718359dad8588a793936f7b0cdcc39aa6663a8
maslowasek0808-eng <maslowasek0808@gmail.com>
2026-05-21 16:14:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -339,7 +339,6 @@ def main():
     app.add_handler(CallbackQueryHandler(handle_callback))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("Бот Лила запущен!")
-    app.run_polling(drop_pending_updates=True)
-
+    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
 if __name__ == "__main__":
     main()
